be1c6d3de5136157a9be10c974800b986bf8707e
seokgu <swheo92@gmail.com>
2022-08-24 01:33:42 +0900

    [2022-08-24] WebPage API Project Commit

---
+++ tos-wp-api/src/main/java/com/tos/toswpapi/TosWpApiApplication.java
@@ -0,0 +1,13 @@
+package com.tos.toswpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TosWpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TosWpApiApplication.class, args);
+    }
+
+}
---
+++ tos-wp-api/src/test/java/com/tos/toswpapi/TosWpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tos.toswpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TosWpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
