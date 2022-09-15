076f9ae5ef94785e792521a802a41dbb37892050
seokgu <swheo92@gmail.com>
2022-09-15 23:05:15 +0900

    Google 설정 파일 Upload

---
+++ tos-wp-api/src/main/java/com/tos/toswpapi/service/GoogleAuthService.java
@@ -0,0 +1,46 @@
+package com.tos.toswpapi.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+@PropertySource("classpath:secret.properties")
+
+@Service
+public class GoogleAuthService {
+
+    @Value("${google.client.auth.endpoint}")
+    private String googleClientAuthEndpoint;
+
+    @Value("${google.client.auth.scope}")
+    private String googleClienAuthScope;
+
+    @Value("${google.client.auth.redirect}")
+    private String googleClientAuthRedirect;
+
+    @Value("${google.client.id}")
+    private String googleClientId;
+
+    public String GetAuthURL(){
+        StringBuffer sb = new StringBuffer();
+        sb.append(googleClientAuthEndpoint + "?");
+        sb.append("scope=" + googleClienAuthScope + "&");
+        sb.append("include_granted_scopes=true&");
+        sb.append("response_type=code&");
+        sb.append("access_type=offline&");
+
+        //if(isRefreshToken)
+            sb.append("prompt=consent&");
+
+        sb.append("state=member");
+        //if(StringUtils.hasText(tarEmail))
+          //  sb.append(":EMAIL_" + tarEmail);
+
+        sb.append("&");
+
+        sb.append("redirect_uri=" + googleClientAuthRedirect + "&");
+        sb.append("client_id=" + googleClientId);
+
+        return sb.toString();
+    }
+}
